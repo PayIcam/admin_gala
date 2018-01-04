98163580f4cf8199f0d8822810c8f62fb1b1d4ed
GregoireGiraud <gregoiregiraud1@gmail.com>
2018-01-04 19:00:16 +0100

    REGARDEZ PAS

--- mdr.php
+++
@@ -1,6 +0,0 @@
-<?php echo realpath('mdr.php');
-
-echo '<br/>';
-echo crypt('#admin-gala', password_hash('#admin-gala', PASSWORD_DEFAULT));
-echo '<br/>';
-echo crypt('nimda', password_hash('#admin-gala', PASSWORD_DEFAULT));
