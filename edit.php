8d3b0c99b737fbfa1e1d3756a276af507e740398
GregoireGiraud <gregoiregiraud1@gmail.com>
2018-01-07 15:05:07 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -5,6 +5,8 @@ session_start();
 if (isset($_SERVER['HTTP_REFERER']))
 {
     $current_url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+    echo $current_url;
+    echo '<br/>'.$_SERVER['HTTP_REFERER'];
     if ($_SERVER['HTTP_REFERER']==$current_url and !isset($_POST['fromicam']) and !isset($_SESSION['retour_edit']))
     {
         $_POST = $_SESSION['page_precedente'];
