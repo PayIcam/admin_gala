1006202679e0f60ab4f8b8e910dcdefb5f8d9170
GregoireGiraud <gregoiregiraud1@gmail.com>
2018-01-13 18:09:25 +0100

    % quotas

--- include/html/parametres/quotas.php
+++ include/html/parametres/quotas.php
@@ -6,6 +6,7 @@
                 <tr>
                     <th class="case_tableau_1">Place</th>
                     <th class="case_tableau_1">Actuellement</th>
+                    <th class="case_tableau_1">Pourcentage</th>
                     <th class="case_tableau_1">Quotas</th>
                 </tr>
             </thead>
@@ -19,6 +20,10 @@
                 <tr>
                     <td class="case_tableau_1"><label for="row"><?php echo htmlspecialchars($row['name']) ?></label></td>
                     <td class="case_tableau_1"><?php echo htmlspecialchars($row['current']);  ?> </td>
+                    <td class="case_tableau_1"><?php $pourcentage =$row['current']/$row['value'];
+                        $pourcentage = round(100*$pourcentage,2);
+                        $pourcentage = color_percentage($pourcentage);
+                        echo $pourcentage; ?> </td>
                     <td class="case_tableau_1"><?php echo htmlspecialchars($row['value']);  ?> </td>
                 </tr>
             <?php } ?>
